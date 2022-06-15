82063412449621542ce9ad412d8ce548d49c2f6d
borismg04 <borismg04@gmail.com>
2022-06-15 15:56:08 -0500

    conexion api

--- Back-End/config/db.js
+++
@@ -1,9 +0,0 @@
-import env from 'dotenv';
-
-const conectarDB =async () => {
-  try{
-
-  }catch(error){
-    console.log(error);
-  }
-}
--- Back-End/index.js
+++ Back-End/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
+
+console.log(process.env.BASE_CLASH_OF_CLANS);
 
 app.listen(4000, () => {
   console.log('🚀 Servidor corriendo en el puerto 4000 🚀');
