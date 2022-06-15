8c02374db1d58dc503c4f34b13cc1e1d9db97fe8
borismg04 <borismg04@gmail.com>
2022-06-15 12:42:20 -0500

    instalacion dependencias bootstrapp

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="primary"
+          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
